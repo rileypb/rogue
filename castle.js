e25273ee8c1c01feac844c822120bdfef24761c8
Phil Riley <rileypb@gmail.com>
2024-11-28 19:03:30 -0500

    save castle generation code

--- castle.js
+++ castle.js
@@ -10,17 +10,38 @@ const DOWN = 1;
 const LEFT = 2;
 const RIGHT = 3;
 
+const FIRST_SUBDIVISION_BORDER = 0;
+const FIRST_SUBDIVISION_INTERIOR = 1;
+
 
 function generateCastle(floorPlan) {
   let subdivision = [{x: 0, y: 0, width: floorPlan.width, height: floorPlan.height, type: TYPE_CASTLE}];
 
-  generateSubdivision(floorPlan, subdivision);
+  let result = generateSubdivision(floorPlan, subdivision);
+
+  // set all tiles to floor
+  for (let x = 0; x < floorPlan.width; x++) {
+	for (let y = 0; y < floorPlan.height; y++) {
+	  floorPlan.set(x,y, new Floor(x, y, FLOOR_MATERIAL_GRASS));
+	}
+  }
+
+//   for (let sub of result) {
+// 	// set tiles on border to FLOOR_MATERIAL_GRASS
+// 	for (let x = sub.x; x < sub.x + sub.width; x++) {
+// 	  for (let y = sub.y; y < sub.y + sub.height; y++) {
+// 		if (x == sub.x || x == sub.x + sub.width - 1 || y == sub.y || y == sub.y + sub.height - 1) {
+// 		  floorPlan.get(x, y).material = FLOOR_MATERIAL_GRASS;
+// 		}
+// 	  }
+// 	}
+//   }
 }
 
 function generateSubdivision(floorPlan, subdivision) {
 	switch (subdivision[0].type) {
 		case TYPE_CASTLE:
-			generateCastleSubdivision(floorPlan, subdivision);
+			return generateCastleSubdivision(floorPlan, subdivision);
 			break;
 	}
 }
@@ -136,29 +157,117 @@ function generateCastleSubdivision(floorPlan, subdivision) {
 	console.log(numberOfComponents);
 	console.log(horizontalSubdivisions * verticalSubdivisions);
 
-	// make some rooms
-	// for each subdivision, make a room
-	// floorPlan.tiles = [];
-	// let x = 1/0;
-	for (let x = 0; x < floorPlan.width; x++) {
-		for (let y = 0; y < floorPlan.height; y++) {
-			floorPlan.get(x, y).material = 3;
-		}
-	}
+	let result = [];
 	for (let sub of combinedSubdivisions) {
-		let roomWidth = sub.width;
-		let roomHeight = sub.height;
-		let roomX = sub.x;
-		let roomY = sub.y;
-		let type = Math.floor(random(4));
-		for (let i = roomX; i < roomX + roomWidth; i++) {
-			for (let j = roomY; j < roomY + roomHeight; j++) {
-				floorPlan.get(i,j).material = type;
-				// floorPlan.tiles[floorPlan.tiles.length - 1].type = type;
-			}
+		if (isBorderSubdivision(sub, floorPlan)) {
+			let recursiveSubdivisions = generateFirstLevelBorderSubdivision(sub, floorPlan)
+			result.push(...recursiveSubdivisions);
 		}
 	}
+
+	return result;
 }
+
+function isBorderSubdivision(subdivision, floorPlan) {
+	return subdivision.x == 0 || subdivision.y == 0 || subdivision.x + subdivision.width == floorPlan.width || subdivision.y + subdivision.height == floorPlan.height;
+}
+
+function generateFirstLevelBorderSubdivision(subdivision, floorPlan) {
+	var x = subdivision.x;
+	var y = subdivision.y;
+	var width = subdivision.width;
+	var height = subdivision.height;
+	let horizontalSubdivisions = Math.floor(random(2, 5));
+	let verticalSubdivisions = Math.floor(random(2, 5));
+	while (width % horizontalSubdivisions != 0) {
+		horizontalSubdivisions = Math.floor(random(2, 5));
+	}
+	while (height % verticalSubdivisions != 0) {
+		verticalSubdivisions = Math.floor(random(2, 5));
+	}
+	
+	let horizontalSubdivisionWidth = width / horizontalSubdivisions;
+	let verticalSubdivisionHeight = height / verticalSubdivisions;
+	let subdivisions = [];
+	for (var i = 0; i < horizontalSubdivisions; i++) {
+		subdivisions.push([]);
+		for (var j = 0; j < verticalSubdivisions; j++) {
+			var x = subdivision.x + i * horizontalSubdivisionWidth;
+			var y = subdivision.y + j * verticalSubdivisionHeight;
+			var width = horizontalSubdivisionWidth;
+			var height = verticalSubdivisionHeight;
+			subdivisions[i].push({u: i, v: j, x: x, y: y, components: [[i, j]], width: width, height: height, type: TYPE_CASTLE});
+		}
+	}
+	let shuffledCoords = [];
+	for (let i = 0; i < horizontalSubdivisions; i++) {
+		for (let j = 0; j < verticalSubdivisions; j++) {
+			shuffledCoords.push([i, j]);
+		}
+	}
+	shuffle(shuffledCoords, true);
+	let combinedSubdivisions = [];
+	let visited = [];
+	for (let i = 0; i < horizontalSubdivisions; i++) {
+		visited.push([]);
+		for (let j = 0; j < verticalSubdivisions; j++) {
+			visited[i].push(false);
+		}
+	}
+	let pathU = Math.floor(random(horizontalSubdivisions));
+	let pathV = Math.floor(random(verticalSubdivisions));
+	visited[pathU][pathV] = true;
+	combinedSubdivisions.push(subdivisions[pathU][pathV]);
+	while (true) {
+		let jumped = false;
+		let newCoords = getNextPosition(pathU, pathV, visited, horizontalSubdivisions, verticalSubdivisions);
+		if (newCoords == null) {
+			newCoords = jump(shuffledCoords, visited);
+			jumped = true;
+		}
+		if (newCoords == null) {
+			break;
+		}
+		let newU = newCoords[0];
+		let newV = newCoords[1];
+		visited[newU][newV] = true;
+		let currentSubdivision = subdivisions[pathU][pathV];
+		let newSubdivision = subdivisions[newU][newV];
+		let orientation = SQUARE;
+		let currentUWidth = currentSubdivision.width / horizontalSubdivisionWidth;
+		let currentVHeight = currentSubdivision.height / verticalSubdivisionHeight;
+		if (currentUWidth > currentVHeight) {
+			orientation = HORIZONTAL;
+		} else if (currentVHeight > currentUWidth) {
+			orientation = VERTICAL;
+		}
+		let newOrientation = SQUARE;
+		if (newSubdivision.u == currentSubdivision.u) {
+			newOrientation = VERTICAL;
+		}
+		if (newSubdivision.v == currentSubdivision.v) {
+			newOrientation = HORIZONTAL;
+		}
+		if (!jumped && (orientation == newOrientation || orientation == SQUARE)) {
+			let combined = combineSubdivisions(currentSubdivision, newSubdivision);
+			for (let i = 0; i < combined.components.length; i++) {
+				subdivisions[combined.components[i][0]][combined.components[i][1]] = combined;
+			}
+			if (combinedSubdivisions.includes(currentSubdivision)) {
+				combinedSubdivisions.splice(combinedSubdivisions.indexOf(currentSubdivision), 1);
+			}
+			if (combinedSubdivisions.includes(newSubdivision)) {
+				combinedSubdivisions.splice(combinedSubdivisions.indexOf(newSubdivision), 1);
+			}
+			combinedSubdivisions.push(combined);
+		}
+		pathU = newU;
+		pathV = newV;
+	}
+
+	return combinedSubdivisions;
+}
+
 
 function combineSubdivisions(subdivision1, subdivision2) {
 	let comps = subdivision1.components.concat(subdivision2.components);
